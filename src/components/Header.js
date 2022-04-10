3cfa213090ac2563102a3b37f68045ab1944d555
Cimorexave <sadeq.s502@gmail.com>
2022-04-10 13:14:12 +0430

    added basic header stylings

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,13 @@ const Header = () => {
   return (
     <div className='header'>
         <h1>Signal Inbox</h1>
-        <input type='text' placeholder='Search...' className='serach-bar' />
-            <i className='serach-icon'></i>
-        <p>Balance: $2,895,102</p>
+        <div>
+          <input type='text' placeholder='Search...' className='search-bar' />
+          <i className='serach-icon'></i>
+        </div>
+        <p className='balance-p'>Balance: $2,895,102</p>
         |
-        <button>Connect Wallet</button>
+        <button className='connect-wallet'>Connect Wallet</button>
         |
         <button><i></i></button>
         <button><i></i></button>
