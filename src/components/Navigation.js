3f3cbc26682eadbf7d8ba59c2da5210eb1f0125f
Cimorexave <sadeq.s502@gmail.com>
2022-04-10 15:00:56 +0430

    added navigation stylings

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,17 +5,18 @@ const Navigation = () => {
   return (
     <div className='navigation'>
         <div className='warning'>
-            <i></i>
+            <i>i</i>
             <p>You can view your purchased or saved analytics here.</p>
         </div>
         <div className='navigation-buttons'>
-            <button>All Inbox</button>
-            <button>Crypto Inbox</button>
-            <button>Forex Inbox</button>
-            <button>Stock Inbox</button>
+            <button className='inbox-btn'>All Inbox</button>
+            <button className='inbox-btn'>Crypto Inbox</button>
+            <button className='inbox-btn'>Forex Inbox</button>
+            <button className='inbox-btn'>Stock Inbox</button>
             <button>Sort by <i></i></button>
             <button>Filter <i></i></button>
         </div>
+        <hr></hr>
     </div>
   )
 }
