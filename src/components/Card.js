8bfb9072743b6d246fe3df4044d093e5c049cc91
Cimorexave <sadeq.s502@gmail.com>
2022-04-11 16:29:10 +0430

    updated modules and finished inner-card fundamental stylings; all page fundamental stylings completed

--- src/components/Card.js
+++ src/components/Card.js
@@ -17,10 +17,20 @@ const Card = () => {
             <p>75354 Point</p>
         </div>
         <div className='inner-card'>
-                <button>Overview</button>
-                <button>Full Analysis</button>
+                <button className='inner-card-btn inner-card-btn-one'>Overview</button>
+                <button className='inner-card-btn inner-card-btn-two'>Full Analysis</button>
+                <div className="inner-card-ps">
+                    <p><i></i>Analysy Type: Premium</p>
+                    <p><i></i>BTCUSDT Bitcoing/Tether</p>
+                    <p><i></i>ExChange: Binance</p>
+                    <p><i></i>TimeFrame: 4H</p>
+                    <p><i></i>Market: Futures</p>
+                    <p><i></i>Method: Swing Trading</p>
+                    <p><i></i>Position: Short</p>
+                    <p><i></i>Leverage: 5X</p>
+                </div>
                 <div className='sticker'>
-                    <i></i>
+                    <i className='sticker-icon'>i</i>
                     <p>Premium Analysis</p>
                     <p>0.10 $</p>
                     <p>Some description</p>
@@ -31,6 +41,9 @@ const Card = () => {
                     <p><i></i>471,292</p>
                     <p>Published in 12-15-2021</p>
                     <p>23:18:51</p>
+                    <i></i>
+                    <i></i>
+                    <i> </i>
                 </footer>
             </div>
     </div>
